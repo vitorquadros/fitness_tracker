04324d57f1a217b3ff4baec457195c4a6a424625
Vitor Quadros <viitorquadros@hotmail.com>
2022-01-19 14:36:00 -0300

    feat: create MainAdapter for recycler view

--- app/src/main/java/dev/vitorquadros/myapplication/MainActivity.java
+++ app/src/main/java/dev/vitorquadros/myapplication/MainActivity.java
@@ -3,26 +3,68 @@ package dev.vitorquadros.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-	private View btnImc;
+//	private View btnImc;
+	private RecyclerView rvMain;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		btnImc = findViewById(R.id.btn_imc);
+		rvMain = findViewById(R.id.main_rv);
+		rvMain.setLayoutManager(new LinearLayoutManager(this));
 
-		btnImc.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent(MainActivity.this, ImcActivity.class);
-				startActivity(intent);
-			}
-		});
+		MainAdapter adapter = new MainAdapter();
+		rvMain.setAdapter(adapter);
+
+		/*btnImc = findViewById(R.id.btn_imc);
+
+		btnImc.setOnClickListener(view -> {
+			Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+			startActivity(intent);
+		});*/
 	}
+
+	private class MainAdapter extends RecyclerView.Adapter<MainViewHolder> {
+
+		@NonNull
+		@Override
+		public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			return new MainViewHolder(getLayoutInflater().inflate(R.layout.main_item, parent, false));
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull MainViewHolder holder, int position) {
+			holder.bind(position);
+		}
+
+		@Override
+		public int getItemCount() {
+			return 15;
+		}
+	}
+
+	private class MainViewHolder extends RecyclerView.ViewHolder {
+
+		public MainViewHolder(@NonNull View itemView) {
+			super(itemView);
+
+		}
+
+		public void bind(int position) {
+			TextView textTest = itemView.findViewById(R.id.textview_test);
+			textTest.setText("Teste de rolagem " + position);
+		}
+	}
+
 }

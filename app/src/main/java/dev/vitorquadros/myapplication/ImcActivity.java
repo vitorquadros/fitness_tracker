9fa5d7a9fae336d7286dba6474ef11ab5c8506b6
Vitor Quadros <viitorquadros@hotmail.com>
2022-01-27 19:27:43 -0300

    feat: run db instance on another thread

--- app/src/main/java/dev/vitorquadros/myapplication/ImcActivity.java
+++ app/src/main/java/dev/vitorquadros/myapplication/ImcActivity.java
@@ -50,9 +50,18 @@ public class ImcActivity extends AppCompatActivity {
                     .setMessage(imcResponseId)
                     .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> {
                     }).setNegativeButton(R.string.save, ((dialog1, which) -> {
-                        long calcId = SqlHelper.getInstance(ImcActivity.this).addItem("imc", result);
-                        if (calcId > 0)
-                            Toast.makeText(ImcActivity.this, R.string.saved, Toast.LENGTH_LONG).show();
+
+                        new Thread(() -> {
+                            long calcId = SqlHelper.getInstance(ImcActivity.this).addItem("imc", result);
+
+                            runOnUiThread(() -> {
+                                if (calcId > 0) {
+                                    Toast.makeText(ImcActivity.this, R.string.saved, Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }).start();
+
+
                     }))
                     .create();
 

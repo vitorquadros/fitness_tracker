14bda071f4062b9a9ec8d989c9cb2ee51a04088c
Vitor Quadros <viitorquadros@hotmail.com>
2022-01-31 10:46:42 -0300

    feat: treat intent on ListCalc

--- app/src/main/java/dev/vitorquadros/myapplication/ListCalcActivity.java
+++ app/src/main/java/dev/vitorquadros/myapplication/ListCalcActivity.java
@@ -3,6 +3,9 @@ package dev.vitorquadros.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class ListCalcActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ListCalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_calc);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            String type = extras.getString("type");
+
+            List<Register> registers =  SqlHelper.getInstance(this).getRegisterBy(type);
+            Log.d("Teste", registers.toString());
+        }
     }
 }
